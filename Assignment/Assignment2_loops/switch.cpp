39fcf777fb1bdcedc9003ef43bc5f4a632ffd615
Nischal Maharjan <burninglykfire@gmail.com>
2021-06-02 15:19:28 +0545

    Program to build a simple calculator using switch Statement

--- Assignment/Assignment2_loops/switch.cpp
+++ Assignment/Assignment2_loops/switch.cpp
@@ -34,6 +34,7 @@ int main() {
             // If the operator is other than +, -, * or /, error message is shown
             cout << "Error! operator is not correct";
             break;
+            
     }
 
     return 0;
