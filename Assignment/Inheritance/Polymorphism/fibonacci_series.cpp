40f101a06d49c8d68a43b0dad026e123e491351c
Nischal <burninglykfire@gmail.com>
2021-07-27 21:33:57 -0700

    operator overloading with pre - increment

---
+++ Assignment/Inheritance/Polymorphism/fibonacci_series.cpp
@@ -0,0 +1,36 @@
+/*Write a program to generate Fibonacci series using operator overloading of ++ operator
+a) for pre increment*/
+
+#include <iostream>
+using namespace std;
+
+class fibonacci
+{
+    private:
+    int n1,n2,n3;
+    public:
+    fibonacci()
+    {
+        n1=0;
+        n2=1;
+        n3=0;
+    }
+    void operator ++()
+    {
+        cout<<n1<<" \t";
+        n3=n1+n2;
+        n1=n2;
+        n2=n3;
+    }
+};
+int main()
+{   
+    int num,i;
+    fibonacci fb;
+    cout<<"How many numbers do you want to print ? ";
+    cin>>num;
+    for(i=0;i<num;i++)
+    {
+    ++fb;
+    }
+}
